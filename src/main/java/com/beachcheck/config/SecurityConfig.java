e779a50f3b1e8e62ed5adf5bc061c2715f5b98b3
notwhitepaper <app.lnob@gmail.com>
2025-10-22 10:46:01 +0900

    SecurityConfig.java에 내용 추가
    requestMatchers에 "/api/**" 추가

--- src/main/java/com/beachcheck/config/SecurityConfig.java
+++ src/main/java/com/beachcheck/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**", "/swagger-ui.html", "/swagger-ui/**", "/api/docs/**").permitAll()
+                        .requestMatchers("/actuator/**", "/swagger-ui.html", "/swagger-ui/**", "/api/**", "/api/docs/**").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
